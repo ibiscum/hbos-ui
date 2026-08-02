bd754b59174926dbb00787dc352ed19def39c7f1
ibiscum <ulf.luecking@gmail.com>
2026-08-02 02:53:35 +0200

    review

--- src/components/Theaudiodb.vue
+++ src/components/Theaudiodb.vue
@@ -27,8 +27,7 @@ import ContentBox from "@/components/ContentBox.vue"
 </script>
 
 <style scoped lang="scss">
-
-// @import '@/assets/scss/service-item';
+@import '@/assets/scss/service-item';
 
 .service-item {
   @include service-item-base;
